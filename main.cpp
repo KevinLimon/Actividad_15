587f27efee45ce5b67c6f70e998dfe3338ff134b
Kevin Limón <kevin.limon7482@alumnos.udg.mx>
2020-11-10 17:07:08 -0600

    Menú funcionando

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<string> cadenas;
+    string opcion;
+
+    while (true)
+    {
+        cout<<"1) Agregar al final"<<endl;
+        cout<<"2) Mostrar"<<endl;
+        cout<<"3) Inicializar"<<endl;
+        cout<<"4) Frente"<<endl;
+        cout<<"5) Ultimo"<<endl;
+        cout<<"6) Ordenar"<<endl;
+        cout<<"7) Insertar"<<endl;
+        cout<<"8) Eliminar"<<endl;
+        cout<<"9) Eliminar ultimo"<<endl;
+        cout<<"0) Salir"<<endl;
+        getline(cin, opcion);
+
+        if (opcion=="1")
+        {
+            /* code */
+        }
+        else if(opcion=="2"){
+
+        }
+        else if(opcion=="3"){
+
+        }
+        else if(opcion=="4"){
+
+        }
+        else if(opcion=="5"){
+
+        }
+        else if(opcion=="6"){
+
+        }
+        else if(opcion=="7"){
+
+        }
+        else if(opcion=="8"){
+
+        }
+        else if(opcion=="9"){
+
+        }
+        else if(opcion=="0"){
+            break;
+        }
+        
+
+    }
+    
+
+    return 0;
+}
